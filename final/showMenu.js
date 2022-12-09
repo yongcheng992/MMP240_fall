a676231886383fa71e0958f0d01d90d7383e30f9
yongcheng992 <93725490+yongcheng992@users.noreply.github.com>
2022-12-09 16:59:42 -0500

    Add files via upload

---
+++ final/showMenu.js
@@ -0,0 +1,9 @@
+function showMenu(){
+        var  menu = document.getElementById("dropmenu");
+
+        if  (menu.style.display==="none" || menu.style.display===''){
+            menu.style.display="block";
+        } else {
+        menu.style.display="none";
+            }
+        }
